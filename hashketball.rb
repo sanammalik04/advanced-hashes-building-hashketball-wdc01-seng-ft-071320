8815a8a2c0920595a6bcf77977560e4c18a115ba
Sanam Malik <sanam.malik04@gmail.com>
2020-07-08 21:44:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 # Write your code here!
 def game_hash
   game = {
-:home => {:team_name => 'Brooklyn Nets', :colors => [], :players =>},
+:home => {:team_name => 'Brooklyn Nets', :colors => ["Black", "White"], :players =>[{:player_name => , :number => , :shoe => , :points => , :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks =>  }]},
 :away => {:team_name =>, :colors =>[], :players =>}
 }
 end
